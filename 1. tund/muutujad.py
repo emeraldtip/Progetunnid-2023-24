d92662cf99db1f588b787824c9cf3c6b1bdf0cc5
emeraldtip <emeraldtipyt@gmail.com>
2023-09-20 16:20:16 +0300

    1. tunni materjal

---
+++ 1. tund/muutujad.py
@@ -0,0 +1,14 @@
+x = 3
+x = 5
+print(x)
+
+
+x = 2
+x = "tere"
+print(x)
+
+x = 3
+a = x
+print(a)
+
+
---
+++ 1. tund/tehted.py
@@ -0,0 +1,24 @@
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+print(2**2)
+
+
+print("siit teine sektsioon")
+a = 3
+b = 4
+print(a+b)
+print("a+b")
+
+
+print("kolmas sektsioon")
+a = "Sünni"
+b = "päev"
+print(a+b)
+
+
+print("neljas sektsioon")
+a="tere"    
+print(a*5)
+
